b865b29300577b8a908b002727c5ab211c406814
Jan Wojtach <wojtachjanek@gmail.com>
2023-04-18 23:39:09 +0200

    [Day 6] Adjust to part 2

--- Day 6/solve.ts
+++ Day 6/solve.ts
@@ -1,11 +1,14 @@
 import fs from "fs";
 
+const TO_BE_DISTINCT = 14;
 const findStartOfMarker = (input: string) =>
   input
     .split("")
     .findIndex(
       (_, idx, arr) =>
-        idx >= 3 && new Set(arr.slice(idx - 3, idx + 1)).size == 4
+        idx >= TO_BE_DISTINCT - 1 &&
+        new Set(arr.slice(idx - (TO_BE_DISTINCT - 1), idx + 1)).size ==
+          TO_BE_DISTINCT
     );
 export default function main(filepath: string) {
   const input = fs.readFileSync(filepath);
